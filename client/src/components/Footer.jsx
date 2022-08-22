bbdf965efe5d9da1f472a636cebe07f97cdab9ea
Dan S <drinkwithdan@gmail.com>
2022-08-22 17:49:46 +1000

    Adding proxy

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Copyright © TinyPOS 2022</p>
+      <a href="https://github.com/drinkwithdan" target="_blank"><p>Github</p></a>
+    </div>
+  )
+}
+
+export default Footer
