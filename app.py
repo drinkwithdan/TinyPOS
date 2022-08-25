ca3f2c7ab29023f4c87fc3cca1467e2c43e34203
Dan S <drinkwithdan@gmail.com>
2022-08-25 13:01:11 +1000

    build route change

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from .db import get_db, close_db
 
 # # # # # INITIALISING # # # # #
 
-app = Flask(__name__, static_folder="./client/build", static_url_path="/")
+app = Flask(__name__, static_folder="./build", static_url_path="/")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.before_request
